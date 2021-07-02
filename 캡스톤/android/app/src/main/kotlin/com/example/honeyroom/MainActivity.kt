33228eda213fb65502029bf4519e4d13bcac1102
taeyang121096 <taeyang121096@naver.com>
2021-07-02 23:48:03 +0900

    upadate

--- 캡스톤/android/app/src/main/kotlin/com/example/honeyroom/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.honeyroom
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- 캡스톤/lib/filter/filter.dart
+++
@@ -1,6 +0,0 @@
-class Filter {
-  Filter({this.name, this.value});
-
-  String name;
-  bool value;
-}
